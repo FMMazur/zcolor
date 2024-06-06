2bc12ac07e4f2e11d461c45c1d7d353ac836fc94
Felipe Muñoz Mazur <felipe.mazur@viveo.com.br>
2024-06-06 11:56:33 -0400

    fix: unused value

--- build.zig
+++ build.zig
@@ -12,7 +12,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    b.addModule("zcolor", .{
+    _ = b.addModule("zcolor", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
